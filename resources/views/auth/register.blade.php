6b5f4c10f78c2bf6243c19b2fc5a4e32fe0dc129
Akira-Fujinami <akira.fujinami@icloud.com>
2023-02-02 21:42:35 +0900

    レイアウト調整

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,10 +4,9 @@
     <div id="register"></div>
     {!! Form::open(['url' => 'register']) !!}
     <h2>新規ユーザー登録</h2>
-    {{ Form::label('username','',['class'=>'username']) }}
-    <div class="top-message">
-        {{ Form::text('username',null,['class' => 'inputer']) }}
-        <div class="message">
+	{{ Form::label('username','',['class'=>'username']) }}
+        {{ Form::text('username',null,['class' => 'inputer-name']) }}
+        <div class="message-name">
 		    @if($errors->has('username'))
 			    @foreach($errors->get('username') as $message)
 				    {{ $message }}<br>
@@ -15,8 +14,8 @@
 		    @endif 
         </div>
         {{ Form::label('mail address','',['class'=>'mail']) }}
-        {{ Form::text('mail_address',null,['class' => 'inputer']) }}
-        <div class="message">
+        {{ Form::text('mail_address',null,['class' => 'inputer-mail']) }}
+        <div class="message-mail">
 		    @if($errors->has('mail_address'))
 			    @foreach($errors->get('mail_address') as $message)
 				    {{ $message }}<br>
@@ -24,24 +23,23 @@
 		    @endif  
         </div>
         {{ Form::label('password','',['class'=>'password']) }}
-        {{ Form::password('password',['class' => 'inputer']) }}
-        <div class="message">
+        {{ Form::password('password',['class' => 'inputer-password']) }}
+        <div class="message-password">
 		    @if($errors->has('password'))
 			    @foreach($errors->get('password') as $message)
 				    {{ $message }}<br>
 			    @endforeach
 		    @endif 
         </div>
-        {{ Form::label('password_confirmation','',['class'=>'password']) }}
-        {{ Form::password('password_confirmation',['class' => 'inputer']) }}
-        <div class="message">
+        {{ Form::label('password_confirmation','',['class'=>'password-confirmation']) }}
+        {{ Form::password('password_confirmation',['class' => 'inputer-confirmation']) }}
+        <div class="message-confirmation">
 		    @if($errors->has('password_confirmation'))
 			    @foreach($errors->get('password_confirmation') as $message)
 				    {{ $message }}<br>
 			    @endforeach
 		    @endif 
         </div>
-    </div>
     {{ Form::submit('REGISTER',['class'=>'outpter']) }}
     <h6><a href="/login">ログイン画面へ戻る</a></h6>
     {!! Form::close() !!}
