619efe5d25ee118707455d87c1fb5a877711f3cb
Akira-Fujinami <akira.fujinami@icloud.com>
2023-02-04 17:05:30 +0900

    レイアウト調整

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -31,7 +31,7 @@ class UsersController extends Controller
         $request->validate([//バリデーションを行う
             'user_name' => 'required|string|min:2|max:12',
             'mail_address' => ['required','email','string','min:5','max:40',Rule::unique('users','mail')->ignore(Auth::id())],
-            'bio' => 'string|max:150',
+            'bio' => 'max:150',
             'password' => 'required|string|min:8|max:20|alpha_dash|confirmed',//confirmedは最初に書く
             'password_confirmation' => 'required|string|min:8|max:20|alpha_dash',//名前_confirmation
             'image'=>'image|mimes:jpeg,png,jpg,gif']);
